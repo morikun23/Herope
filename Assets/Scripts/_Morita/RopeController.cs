699829c7583b4603e858d13d3833cc681e3eba77
MoritaSuguru <morisug4645@gmail.com>
2017-10-09 02:54:33 +0900

    【ロープ】プレイヤーの座標に結ばれる程度で実装

---
+++ Assets/Scripts/_Morita/RopeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Herope {
+	public class RopeController : MonoBehaviour {
+
+		private LineRenderer m_lineRenderer;
+
+		public Transform m_startPos;
+		public Transform m_endPos;
+
+		// Use this for initialization
+		void Start() {
+			m_lineRenderer = GetComponent<LineRenderer>();
+			m_lineRenderer.numPositions = 2;
+			m_lineRenderer.SetPosition(0 , m_startPos.position);
+		}
+
+		// Update is called once per frame
+		void Update() {
+			
+			m_lineRenderer.SetPosition(1 , m_endPos.position);
+		}
+	}
+}
