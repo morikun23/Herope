f783d7f35fd9d1bbabf934df398c62f0a06118f8
MoritaSuguru <morisug4645@gmail.com>
2017-10-09 02:03:21 +0900

    【モック】重力を左方向にできました

---
+++ Assets/Scripts/_Morita/PhysicsManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Herope.Morita {
+	public class PhysicsManager : MonoBehaviour {
+
+		// Use this for initialization
+		void Start() {
+			Physics2D.gravity = Vector2.left * 9.81f;
+		}
+
+		// Update is called once per frame
+		void Update() {
+
+		}
+	}
+}
