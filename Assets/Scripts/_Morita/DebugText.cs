e6a7f1ad03f5e39b4e092caa9cf7824af4907910
MoritaSuguru <morisug4645@gmail.com>
2017-10-09 02:36:07 +0900

    【プレイヤー】デバッグ用ウィンドウを用意

---
+++ Assets/Scripts/_Morita/DebugText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Herope.Morita {
+	public class DebugText : MonoBehaviour {
+
+		Text m_text;
+
+		Player player;
+
+		// Use this for initialization
+		void Start() {
+			m_text = GetComponent<Text>();
+			player = FindObjectOfType<Player>();
+		}
+
+		// Update is called once per frame
+		void Update() {
+
+			string state = player.GetCurrentState().ToString();
+			m_text.text = state;
+		}
+	}
+}
---
+++ Assets/Scripts/_Morita/JumpButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Herope.Morita {
+	public class JumpButton : MonoBehaviour {
+
+		Player player;
+
+		[Range(0 , 1)]
+		[SerializeField]
+		float jumpPower;
+
+		// Use this for initialization
+		void Start() {
+			player = FindObjectOfType<Player>();
+		}
+
+		// Update is called once per frame
+		void Update() {
+			
+		}
+
+		public void OnButtonDown() {
+			player.m_chargePower = jumpPower;
+			player._StateTransition(new PlayerJumpState());
+		}
+	}
+}
