93c21791245739596537364199ba9bbff73c6267
MoritaSuguru <morisug4645@gmail.com>
2017-10-11 10:00:40 +0900

    【ギミック】スクロールする基底クラスを作成

---
+++ Assets/Scripts/Gimmick/FlowingObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Herope {
+	public class FlowingObject : ToyBox.ObjectBase {
+
+		public float m_speed;
+
+		// Use this for initialization
+		protected virtual void Start() {
+
+		}
+
+		// Update is called once per frame
+		protected virtual void Update() {
+			m_transform.position += Vector3.up * m_speed;
+		}
+	}
+}
