f02e66a935796e7fb23dc7ff1143b50c516051ce
morita.suguru <morisug4645@gmail.com>
2017-10-11 14:52:21 +0900

    【ギミック】炎のギミックを実装（あたり判定とダメージ処理のみ）

---
+++ Assets/Scripts/Gimmick/FireArea.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Herope {
+	public class FireArea : FlowingObject {
+
+		// Use this for initialization
+		protected override void Start() {
+
+		}
+
+		// Update is called once per frame
+		protected override void Update() {
+			base.Update();
+		}
+
+		protected virtual void OnTriggerEnter2D(Collider2D arg_colldier) {
+			if(arg_colldier.gameObject.layer == LayerMask.NameToLayer("Player")) {
+				Player player = arg_colldier.transform.parent.GetComponent<Player>();
+				player.Damage(1);
+			}
+		}
+	}
+}
