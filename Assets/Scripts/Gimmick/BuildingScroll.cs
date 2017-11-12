1f14aabe3550326ba862252f18443f618eed282b
kuno.naoyuki <naonosuke8267@gmail.com>
2017-11-12 19:30:41 +0900

    【ステージ】スクロール機能追加、それに合わせてプレーヤーの動きをそれっぽく最適化

---
+++ Assets/Scripts/Gimmick/BuildingScroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Herope;
+
+namespace Herope{
+	public class BuildingScroll : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+
+			transform.Translate (new Vector3(0,ScrollManager.Instance.GetScrollSpeed(),0));
+		}
+	}
+}
