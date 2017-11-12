fd3786179511749de3797f936bd2ed8b779e90f9
morita.suguru <morisug4645@gmail.com>
2017-11-13 02:06:25 +0900

    【プレイ時間】ゲーム内のプレイ時間を計測するようタイマーを実装

---
+++ Assets/Scripts/GameScene/Main/GameTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Herope {
+	public class GameTimer {
+
+		private float m_elapsedTime;
+
+		public float CurrentTime {
+			get { return m_elapsedTime; }
+		}
+
+		public void Initialize() {
+			m_elapsedTime = 0;
+		}
+
+		public void UpdateByFrame() {
+			m_elapsedTime += Time.deltaTime;
+		}
+	}
+}
