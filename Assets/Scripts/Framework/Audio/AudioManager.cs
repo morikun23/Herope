c1fab202e797a48256c14b126a4938209374eb24
kuno.naoyuki <naonosuke8267@gmail.com>
2017-11-13 23:37:20 +0900

    【フレームワーク】AudioManagerを最新版に更新

--- Assets/Scripts/Framework/Audio/AudioManager.cs
+++ Assets/Scripts/Framework/Audio/AudioManager.cs
@@ -12,27 +12,6 @@ using System.Linq;
 
 namespace ToyBox {
 	public class AudioManager : MonoBehaviour {
-		
-		/// <summary>
-		/// コンストラクタ
-		/// </summary>
-		private AudioManager() { }
-
-		// Singletonインスタンス
-		private static AudioManager m_instance;
-
-		/// <summary>
-		/// インスタンスの取得
-		/// nullだった場合、自動的に生成させる
-		/// </summary>
-		public static AudioManager Instance {
-			get {
-				if (m_instance == null) {
-					m_instance = new GameObject("AudioManager").AddComponent<AudioManager>();
-				}
-				return m_instance;
-			}
-		}
 
 		//Se管理クラス
 		private IAudioList m_seList;
@@ -40,11 +19,16 @@ namespace ToyBox {
 		//Bgm管理クラス
 		private IAudioList m_bgmList;
 
+		//Audioフォルダの固定パス
+		private const string FOLDER = "Audio/";
+		private const string BGM = "BGM/";
+		private const string SE = "SE/";
+
 		/// <summary>
 		/// 初期化
 		/// </summary>
 		public void Initialize() {
-			m_seList = new SeList(10);
+			m_seList = new SeList(30);
 			m_seList.Initialize();
 			
 			m_bgmList = new BgmList(1);
@@ -59,20 +43,81 @@ namespace ToyBox {
 		/// <param name="arg_audioClip">再生するオーディオ</param>
 		/// <returns>生成されたAudioSource</returns>
 		public AudioSource CreateBgm(AudioClip arg_audioClip) {
+			if (arg_audioClip == null) {
+				Debug.LogError ("空のAudioClipが渡されました");
+				return null;
+			}
+
 			AudioSource freeAudioSource = m_bgmList.GetAudioSource();
+			if(freeAudioSource == null) {
+				Debug.LogError ("AudioSourceの容量不足です");
+				return null; 
+			}
 			freeAudioSource.clip = arg_audioClip;
 			return freeAudioSource;
 		}
-		
+
+		/// <summary>
+		/// BGMを追加する
+		/// ※Resources内の専用フォルダにファイルを入れてください
+		/// </summary>
+		/// <param name="arg_clipName">再生するオーディオのファイル名</param>
+		/// <returns></returns>
+		public AudioSource CreateBgm(string arg_clipName) {
+
+			if (string.IsNullOrEmpty (arg_clipName)) {
+				Debug.LogError ("文字列が空です");
+				return null;
+			}
+
+			AudioClip clip = Resources.Load<AudioClip>(FOLDER + BGM + arg_clipName);
+			if (clip == null) {
+				Debug.LogError ("ファイル取得失敗");
+				return null;
+			}
+			return this.CreateBgm(clip);
+		}
+
 		/// <summary>
 		/// SEを追加する
 		/// </summary>
 		/// <param name="arg_audioClip">再生するオーディオ</param>
 		/// <returns>生成されたAudioSource</returns>
 		public AudioSource CreateSe(AudioClip arg_audioClip) {
+
+			if (arg_audioClip == null) {
+				Debug.LogError ("空のAudioClipが渡されました");
+				return null;
+			}
+
 			AudioSource freeAudioSource = m_seList.GetAudioSource();
+			if(freeAudioSource == null) {
+				Debug.LogError ("AudioSourceの容量不足です");
+				return null; 
+			}
 			freeAudioSource.clip = arg_audioClip;
 			return freeAudioSource;
+		}
+
+		/// <summary>
+		/// SEを追加する
+		/// ※Resources内の専用フォルダにファイルを入れてください
+		/// </summary>
+		/// <param name="arg_clipName"></param>
+		/// <returns></returns>
+		public AudioSource CreateSe(string arg_clipName) {
+			if (string.IsNullOrEmpty (arg_clipName)) {
+				Debug.LogError ("文字列が空です");
+				return null;
+			}
+				
+
+			AudioClip clip = Resources.Load<AudioClip>(FOLDER + SE + arg_clipName);
+			if (clip == null) {
+				Debug.LogError ("ファイル取得失敗");
+				return null;
+			}
+			return this.CreateSe(clip);
 		}
 
 		/// <summary>
