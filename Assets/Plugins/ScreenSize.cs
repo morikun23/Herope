8725c3c11950f35516fbe948c83d7a01e0d2452b
morita.suguru <morisug4645@gmail.com>
2017-11-15 03:07:20 +0900

    【画面サイズ】FullScreenモードに設定

---
+++ Assets/Plugins/ScreenSize.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSize {
+	[RuntimeInitializeOnLoadMethod]
+	static void OnRuntimeMethodLoad() {
+		Screen.SetResolution(1920 , 1080 , false , 60);
+	}
+}
