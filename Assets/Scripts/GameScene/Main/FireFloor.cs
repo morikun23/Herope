6dc0a4ca6476cf4d20fd558eebe0cf9d2dc2b99d
kuno.naoyuki <naonosuke8267@gmail.com>
2017-11-14 10:49:17 +0900

    【ステージ】ビルのベランダが確率で伸び、さらに確率でその先に炎が置かれるように

---
+++ Assets/Scripts/GameScene/Main/FireFloor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFloor : MonoBehaviour {
+
+	[SerializeField]
+	GameObject obj_advFloor;
+	[SerializeField]
+	GameObject obj_advFire;
+
+	// Use this for initialization
+	void Start () {
+		if (Random.Range (0, 100) > 40) {
+			obj_advFloor.SetActive (false);
+			obj_advFire.SetActive (false);
+		} else if(Random.Range (0, 100) > 50){
+			obj_advFire.SetActive (false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
